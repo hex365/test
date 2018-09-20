931ca2b86164ab841ca080995d66933b933175da
your name <you@example.com>
2018-09-20 18:35:20 +0800

    https://www.taniarascia.com/getting-started-with-react/ and create a new branch

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const Hello = () => <h2>Hello World from HSBC</h2>
-const Image = ({src, title, width, height}) => {
-  src = src || 'https://www.w3schools.com/html/pic_trulli.jpg';
-  title = title ||  'Image';
-  width = width || "500";
-  height = height || "333";
-  return (
-  <img src={src} alt={title} width={width} height={height}></img>
-  ) 
-}
+import Table from './table';
+import Form from './form';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Hello/>
-        <Image title="This is a image from w3school"/> 
-        <br/>
-        <Image src="https://www.w3schools.com/html/img_girl.jpg" />
-        <br/>
-        <Image src="https://www.w3schools.com/html/img_chania.jpg" />
-      </div>
-    );
-  }
+    state = {
+        characters: []
+    };  
+    
+    removeCharacter = index => {
+        const { characters } = this.state;
+    
+        this.setState({
+            characters: characters.filter((character, i) => { 
+                return i !== index;
+            })
+        });
+    }
+
+    handleChange = event => {
+        const {name, value} = event.target;
+    
+        this.setState({
+            [name] : value
+        });
+    }
+
+    handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]});
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Table 
+                    characterData={this.state.characters} 
+                    removeCharacter={this.removeCharacter}
+                />
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+        );
+    }
 }
 
 export default App;
